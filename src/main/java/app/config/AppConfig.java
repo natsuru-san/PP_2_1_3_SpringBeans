f1becafff1e60eb1d3e49f10caf2e152985d0066
natsuru <natsuru@null.net>
2022-06-05 21:22:20 +0300

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,23 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean("Dog")
+    public Animal getDog() {
+        return new Dog();
+    }
+
+    @Bean("Timer")
+    public Timer getTimer() {
+        return new Timer();
+    }
+}
